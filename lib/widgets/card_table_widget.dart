a3af475468455780449dbf53ae29fd936d90dcf5
hadiazb <hadiazb@unal.edu.co>
2021-11-11 15:17:29 -0500

    end project

--- lib/widgets/card_table_widget.dart
+++ lib/widgets/card_table_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -18,11 +20,6 @@ class CardTable extends StatelessWidget {
             color: Colors.pinkAccent,
             text: 'Transport',
           ),
-          _SingleCard(
-            icon: Icons.shield,
-            color: Colors.amberAccent,
-            text: 'Shopping',
-          )
         ]),
         TableRow(children: [
           _SingleCard(
@@ -35,6 +32,25 @@ class CardTable extends StatelessWidget {
             color: Colors.lime,
             text: 'Address',
           ),
+        ]),
+        TableRow(children: [
+          _SingleCard(
+            icon: Icons.g_mobiledata,
+            color: Colors.yellowAccent,
+            text: 'Airtm',
+          ),
+          _SingleCard(
+            icon: Icons.safety_divider_sharp,
+            color: Colors.greenAccent,
+            text: 'Bill',
+          ),
+        ]),
+        TableRow(children: [
+          _SingleCard(
+            icon: Icons.shield,
+            color: Colors.amberAccent,
+            text: 'Shopping',
+          ),
           _SingleCard(
             icon: Icons.earbuds,
             color: Colors.blueGrey,
@@ -43,37 +59,15 @@ class CardTable extends StatelessWidget {
         ]),
         TableRow(children: [
           _SingleCard(
-            icon: Icons.g_mobiledata,
-            color: Colors.yellowAccent,
-            text: 'Airtm',
-          ),
-          _SingleCard(
-            icon: Icons.safety_divider_sharp,
-            color: Colors.greenAccent,
-            text: 'Bill',
-          ),
-          _SingleCard(
             icon: Icons.panorama_photosphere,
             color: Colors.redAccent,
             text: 'Pappers',
-          )
-        ]),
-        TableRow(children: [
-          _SingleCard(
-            icon: Icons.g_mobiledata,
-            color: Colors.yellowAccent,
-            text: 'Airtm',
           ),
           _SingleCard(
-            icon: Icons.safety_divider_sharp,
-            color: Colors.greenAccent,
-            text: 'Bill',
+            icon: Icons.pie_chart_sharp,
+            color: Colors.lime,
+            text: 'Address',
           ),
-          _SingleCard(
-            icon: Icons.panorama_photosphere,
-            color: Colors.redAccent,
-            text: 'Pappers',
-          )
         ]),
       ],
     );
@@ -91,27 +85,47 @@ class _SingleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return _CardBackground(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        CircleAvatar(
+          backgroundColor: color,
+          child: Icon(
+            icon,
+            size: 30,
+          ),
+        ),
+        Text(
+          text,
+          style: TextStyle(color: color, fontSize: 18),
+        ),
+      ],
+    ));
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  const _CardBackground({Key? key, required this.child}) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10),
-      height: 160,
-      decoration: BoxDecoration(
-          color: const Color.fromRGBO(62, 66, 107, 0.7),
-          borderRadius: BorderRadius.circular(20)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            child: Icon(
-              icon,
-              size: 30,
-            ),
+      margin: const EdgeInsets.all(20),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 160,
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(62, 66, 107, 0.7),
+                borderRadius: BorderRadius.circular(20)),
+            child: child,
           ),
-          Text(
-            text,
-            style: TextStyle(color: color, fontSize: 18),
-          ),
-        ],
+        ),
       ),
     );
   }
